6953d121ceab8dd63afd877e665ee8bfa1bd6314
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-11-22 16:14:35 +0800

    upload

--- packages/schilytools/build.sh
+++ packages/schilytools/build.sh
@@ -111,6 +111,7 @@ termux_step_configure() {
 	export ac_cv_sizeof_char_p=8
 	export ac_cv_sizeof_float=4
 	export ac_cv_sizeof_long_double=16
+	export ac_cv_func_mlock=no
 }
 
 termux_step_make() {
